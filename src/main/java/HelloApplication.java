80b0ae7126a9b7d080230641a4aa6b08dde3b2b6
shukuchi23 <vaizerd2312@gmail.com>
2022-04-25 22:36:44 +0300

    хуета какая-та)

--- src/main/java/HelloApplication.java
+++ src/main/java/HelloApplication.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,24 +17,26 @@ import java.util.Objects;
 
 public class HelloApplication extends Application {
 
-  @Override
-  public void start(Stage stage) throws IOException {
-    try (InputStream resourceAsStream = getClass().getResourceAsStream("hello-view.fxml")){
-      FXMLLoader fxmlLoader = new FXMLLoader();
+    @Override
+    public void start(Stage stage) throws IOException {
+        try (InputStream resourceAsStream = getClass().getResourceAsStream("hello-view.fxml")) {
+            FXMLLoader fxmlLoader = new FXMLLoader();
 //      Pane panel = (Pane) fxmlLoader.load(resourceAsStream);
 //      fxmlLoader.setLocation(url);
 //    InputStream inputStream = HelloApplication.class.getResource("/hello-view.fxml").openStream();
-      Parent root = fxmlLoader.load(resourceAsStream);
-      final ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("img/jutsu.png"))));
+//      Parent root = fxmlLoader.load(resourceAsStream);
+            final ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("img/jutsu.png"))));
+            Parent root = new VBox(imageView);
 
-      Scene scene = new Scene(root);
-      stage.setTitle("Hello!");
-      stage.setScene(scene);
-      stage.show();
+//      new VBox()
+            Scene scene = new Scene(root);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    launch();
-  }
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- src/main/java/fxcomponent/FxSavePoint.java
+++ src/main/java/fxcomponent/FxSavePoint.java
@@ -1,12 +1,14 @@
-package model;
+package fxcomponent;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import model.SavePoint;
 
 /**
  * @author Karimov Evgeniy
